3cfaa52a729b3f8e086a5415184808b1acfa9627
Unknown <markus.n252@stu.nebo.edu>
2018-03-12 12:38:25 -0600

    color JS

---
+++ Color_Changer/Color_Changer.js
@@ -0,0 +1,32 @@
+//Variables//
+//This grabs the id of the first modal and sets it equal to modal
+var modal = document.getElementById("modal");
+//This grabs the button with the id "change"
+//This allows us to call a function with a click later
+var changeBtn = document.getElementById("change");
+//This grabs the preview
+var preview = document.getElementById("preview");
+
+//Functions//
+
+??Function to set Preview Boxes//
+
+/This sets the Preview Box color in the modal to the currently selected color
+function Preview() {
+    //This gets the currently selected color's value and sets it equal to color
+    color = document.querySelector('input[name="colors"]:checked').value;
+    //Log what color
+    console.log("The color is " + color);
+    //Change preview color
+    preview.style.background = color;
+}
+
+//Function to change Color//
+
+//Change the Color
+function changeColor() {
+    //Log what the color was changed to
+    console.log("The color is: " + color);
+    //Cange the bachground color
+    document.body.style.background = color;
+}
