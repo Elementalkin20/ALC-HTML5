3a1eb76880da28289dda0e3c547bb4204698a032
Unknown <markus.n252@stu.nebo.edu>
2018-03-27 13:09:17 -0600

    Finish color

--- Color_Changer/Color_Changer.js
+++ Color_Changer/Color_Changer.js
@@ -9,9 +9,9 @@ var preview = document.getElementById("preview");
 
 //Functions//
 
-??Function to set Preview Boxes//
+//Function to set Preview Boxes//
 
-/This sets the Preview Box color in the modal to the currently selected color
+//This sets the Preview Box color in the modal to the currently selected color
 function Preview() {
     //This gets the currently selected color's value and sets it equal to color
     color = document.querySelector('input[name="colors"]:checked').value;
@@ -29,4 +29,35 @@ function changeColor() {
     console.log("The color is: " + color);
     //Cange the bachground color
     document.body.style.background = color;
+}
+
+//Function to open modal//
+
+//Open color changing modal
+changeBtn.onclick = function() {
+    modal.style.display = "grid";
+}
+
+//Function to close modal//
+
+//When the user clicks on the close button in modal it closes it
+function Close() {
+    //Close modal
+    modal.style.display = "none";
+    //Reset Preview
+    preview.style.background = "#1a1a1a";
+    //Unselect any currently selected color
+    document.querySelector('input[name="color"]:checked').checked = false;
+}
+
+//Close modal on window click
+window.onclick = function(event) {
+    if (event.target == modal) {
+        //Close modal
+        modal.style.display = "none";
+        //Reset Preview
+        preview.style.background = "#1a1a1a";
+        //Unselect any currently selected color
+        document.querySelector('input[name="colors"]:checked').checked = false;
+    }
 }
